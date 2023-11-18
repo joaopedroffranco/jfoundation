3f802a8ab6c3ea02c767dfc8ed821086c5a3907c
Joao Pedro Fabiano Franco <joao.franco@deliveryhero.com>
2023-11-18 16:07:03 +0100

    init project

---
+++ JFoudationRepository/JFoudationRepository.h
@@ -0,0 +1,18 @@
+//
+//  JFoudationRepository.h
+//  JFoudationRepository
+//
+//  Created by João Pedro Fabiano Franco on 18.11.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JFoudationRepository.
+FOUNDATION_EXPORT double JFoudationRepositoryVersionNumber;
+
+//! Project version string for JFoudationRepository.
+FOUNDATION_EXPORT const unsigned char JFoudationRepositoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JFoudationRepository/PublicHeader.h>
+
+
